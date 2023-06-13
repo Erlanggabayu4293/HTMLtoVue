971bbec35168bceff3769ff8dfe4ddb851fbc4c9
Erlanggabayu4293 <101629639+Erlanggabayu4293@users.noreply.github.com>
2023-06-13 22:14:35 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Navbar from '../src/components/Navbar.vue'
+  import Main from '../src/components/Main.vue'
+
+</script>
+
+<template>
+  <Navbar/>
+  <Main/>
+
+</template>
